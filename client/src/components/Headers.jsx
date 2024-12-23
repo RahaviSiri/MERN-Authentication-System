8305a496762b1d58dc8966ff90611832eecb276b
RahaviSiri <rahavi24siri@gmail.com>
2024-12-23 14:17:50 +0530

    Add nav bar and header and other react app, css

---
+++ client/src/components/Headers.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Headers = () => {
+  return (
+    <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
+        <img src={assets.header_img} alt=""  className='w-36 h-36 rounded-full mb-6'/>
+        <h1 className='flex gap-2 items-center text-xl sm:text-3xl font-medium mb-3'>Hey Developer 
+            <img src={assets.hand_wave} alt="" className='w-8 aspect-square'/>
+        </h1>
+        <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to Our Service</h2>
+        <p className='mb-8 max-w-md'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ducimus, unde, iure obcaecati recusandae </p>
+        <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
+    </div>
+  )
+}
+
+export default Headers
